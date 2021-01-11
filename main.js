b898b30fb35433dd068ab081e7603c21865a55ac
elior-gispan <eliorg82@gmail.com>
2021-01-11 21:50:49 +0200

    dividing to functions and another changes

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ var pauseButton = document.querySelector(".pause");
 var minutesInput = document.querySelector(".minutes");
 var secondsInput = document.querySelector(".seconds");
 var timeDiv = document.querySelector(".time");
-var catDiv = document.querySelector("#cat");
+var catDiv = document.querySelector(".cat");
 var intervalID;
-var gameOver = false;
 
 function initialTimer(min, sec) {
   minutesInput.value = min;
@@ -18,15 +17,17 @@ function initialTimer(min, sec) {
 
 function setTimer() {
   min.innerText =
-    minutesInput.value >= 0 ? ("0" + minutesInput.value).slice(-2) : "00";
+    minutesInput.value >= 0 ? ("00" + minutesInput.value).slice(-2) : "00";
   sec.innerText =
-    secondsInput.value >= 0 ? ("0" + secondsInput.value).slice(-2) : "00";
+    secondsInput.value >= 0 ? ("00" + secondsInput.value).slice(-2) : "00";
+  catDiv.style.display = "none";
+  timeDiv.style.display = "flex";
 }
 
 initialTimer(0, 3);
 
-minutesInput.onchange = setTimer;
-secondsInput.onchange = setTimer;
+minutesInput.oninput = setTimer;
+secondsInput.oninput = setTimer;
 
 var spinnerHtml =
   "<div class='lds-spinner'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>";
@@ -36,79 +37,59 @@ stopButton.addEventListener("click", stopTimer);
 pauseButton.addEventListener("click", pauseTimer);
 
 async function showCat() {
+  timeDiv.style.display = "none";
+  catDiv.style.display = "flex";
+  catDiv.innerHTML = spinnerHtml;
   var res = await fetch("https://aws.random.cat/meow");
-  timeDiv.innerHTML = spinnerHtml;
+  //   timeDiv.innerHTML = spinnerHtml;
   var json = await res.json();
-  timeDiv.innerHTML = `<img src="${json.file}" width="100em" height="100em">`;
-  function showCat() {
-    fetch("https://aws.random.cat/meow")
-      .then(function (res) {
-        timeDiv.style.display = "none";
-        catDiv.style.display = "flex";
+  //   timeDiv.innerHTML = `<img src="${json.file}" width="100em" height="100em">`;
+  catDiv.innerHTML = `<img src="${json.file}" width="100em" height="100em">`;
+  console.log("cat", catDiv);
+}
 
-        catDiv.innerHTML = spinnerHtml;
-        return res.json();
-      })
-      .then(function (json) {
-        catDiv.innerHTML = `<img src="${json.file}" width="100em" height="100em">`;
-        // console.log("cat", catDiv);
-        setTimeout(function () {
-          catDiv.innerHTML = "";
-          catDiv.style.display = "none";
-          timeDiv.style.display = "flex";
-        }, 5000);
-      });
-  }
+function sayMeow() {
+  var meow = new Audio();
+  meow.src = "/meow.mp3";
+  meow.play();
+}
 
-  function sayMeow() {
-    var meow = new Audio();
-    meow.src = "/elior-gispan.github.io\\meow.mp3";
-    meow.play();
-  }
+function buttonsDisabledToggle(boolean) {
+  startButton.disabled = boolean;
+  minutesInput.disabled = boolean;
+  secondsInput.disabled = boolean;
+  startButton.style.backgroundColor = boolean ? "#cccccc" : "rgb(68, 199, 68)";
+}
 
-  function stopTimer() {
-    clearInterval(intervalID);
-    if (gameOver) {
-      // TODO hiding the image of the cat;
-    }
-    minutesInput.disabled = false;
-    secondsInput.disabled = false;
-    setTimer();
-    startButton.disabled = false;
-    startButton.style.backgroundColor = "rgb(68, 199, 68)";
-  }
+function stopTimer() {
+  clearInterval(intervalID);
+  buttonsDisabledToggle(false);
+  setTimer();
+}
 
-  function pauseTimer() {
-    clearInterval(intervalID);
-    startButton.disabled = false;
-    startButton.style.backgroundColor = "rgb(68, 199, 68)";
-    minutesInput.disabled = false;
-    secondsInput.disabled = false;
-  }
+function pauseTimer() {
+  clearInterval(intervalID);
+  buttonsDisabledToggle(false);
+}
 
-  function startTimer() {
-    startButton.disabled = true;
-    startButton.style.backgroundColor = "#cccccc";
-    minutesInput.disabled = true;
-    secondsInput.disabled = true;
-    intervalID = setInterval(() => {
-      var seconds = +sec.innerText;
-      var minutes = +min.innerText;
-      if (seconds > 0) {
-        seconds--;
+function startTimer() {
+  buttonsDisabledToggle(true);
+  intervalID = setInterval(() => {
+    var seconds = +sec.innerText;
+    var minutes = +min.innerText;
+    if (seconds > 0) {
+      seconds--;
+    } else {
+      if (minutes > 0) {
+        minutes--;
+        seconds = 59;
+        min.innerText = ("00" + minutes).slice(-2);
       } else {
-        if (minutes > 0) {
-          minutes--;
-          seconds = 59;
-          min.innerText = ("0" + minutes).slice(-2);
-        } else {
-          stopTimer();
-          sayMeow();
-          showCat();
-          gameOver = true;
-        }
+        stopTimer();
+        sayMeow();
+        showCat();
       }
-      sec.innerText = ("0" + seconds).slice(-2);
-    }, 1000);
-  }
+    }
+    sec.innerText = ("00" + seconds).slice(-2);
+  }, 1000);
 }
